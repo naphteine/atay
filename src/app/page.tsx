369bc07ec93170121928d6a3082119965f543193
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-08-30 15:41:13 +0300

    Initialize Next

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Atay</h1>
+      <h2>Coming soon.</h2>
+    </main>
+  );
+}
