486e908a5a55ff89c7f6db50af02a4dc0be630f2
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-09-01 16:10:56 +0300

    Add logo and icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Atay</h1>
+      <Image src="/logo.svg" alt="Atay" width={320} height={128} />
       <h2>Coming soon.</h2>
     </main>
   );
