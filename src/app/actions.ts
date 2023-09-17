8c35a0d41820fe030d893d0830642d9ab1e0b640
gguilt <58526116+naphteine@users.noreply.github.com>
2023-09-17 20:28:29 +0300

    Try-catch login request

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,34 +1,38 @@
-'use server';
+"use server";
 
-import { redirect } from 'next/navigation';
-import PocketBase from 'pocketbase';
-import { cookies } from 'next/headers';
+import { redirect } from "next/navigation";
+import PocketBase from "pocketbase";
+import { cookies } from "next/headers";
 
 export async function login(formData: FormData) {
-  const email = formData.get('email') as string;
-  const password = formData.get('password') as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
 
   // TODO: server-side validation
 
   const pb = new PocketBase(process.env.POCKETBASE_URL);
 
-  const { token, record: model } = await pb
-    .collection('atay_users')
-    .authWithPassword(email, password);
+  try {
+    const { token, record: model } = await pb
+      .collection("atay_users")
+      .authWithPassword(email, password);
 
-  const cookie = JSON.stringify({ token, model });
+    const cookie = JSON.stringify({ token, model });
 
-  cookies().set('pb_auth', cookie, {
-    secure: true,
-    path: '/',
-    sameSite: 'strict',
-    httpOnly: true,
-  });
-
-  redirect('/dashboard');
+    cookies().set("pb_auth", cookie, {
+      secure: true,
+      path: "/",
+      sameSite: "strict",
+      httpOnly: true,
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    redirect("/dashboard");
+  }
 }
 
 export async function logout() {
-  cookies().delete('pb_auth');
-  redirect('/');
+  cookies().delete("pb_auth");
+  redirect("/");
 }
