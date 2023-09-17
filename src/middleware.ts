9140874510661710b034c5366c3808bc4c3b61fb
gguilt <58526116+naphteine@users.noreply.github.com>
2023-09-17 18:16:32 +0300

    Don't open login if already logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,24 @@ import { NextRequest, NextResponse } from 'next/server';
 import { isTokenExpired } from 'pocketbase';
 
 export function middleware(request: NextRequest) {
-  // You can also export a `config.matcher` array,
-  // but i believe this way is more straightforward and scalable.
   if (request.nextUrl.pathname.startsWith('/dashboard')) {
     const authCookie = request.cookies.get('pb_auth');
     const token = authCookie?.value ? JSON.parse(authCookie.value).token : null;
 
     // If there's no token or it's expired, redirect to login page.
     if (!token || isTokenExpired(token)) {
+      const url = request.nextUrl.clone();
+      url.pathname = '/';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  if (request.nextUrl.pathname.startsWith('/login')) {
+    const authCookie = request.cookies.get('pb_auth');
+    const token = authCookie?.value ? JSON.parse(authCookie.value).token : null;
+
+    // If there's token and it's not expired, don't open login page.
+    if (token && !isTokenExpired(token)) {
       const url = request.nextUrl.clone();
       url.pathname = '/';
       return NextResponse.redirect(url);
