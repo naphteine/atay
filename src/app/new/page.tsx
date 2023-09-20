6a294751e2b740d61949b21de858b2629024f451
gguilt <58526116+naphteine@users.noreply.github.com>
2023-09-21 00:19:09 +0300

    We can add new books now

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,17 +1,106 @@
-import { TextField } from "@mui/material";
+"use client";
+
+import { TextField, Button } from "@mui/material";
+import { useState, ChangeEvent, FormEvent } from "react";
+import pb from "@/lib/pocketbase";
 
 const New = () => {
+  const [formData, setFormData] = useState({
+    bookName: "",
+    bookISBN: "",
+    bookPage: 0,
+    bookCover: null as File | null,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleCoverChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      bookCover: file || null,
+    }));
+  };
+
+  const newBookSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { bookName, bookISBN, bookPage, bookCover } = formData;
+
+    if (!pb.authStore.model) {
+      return alert("You are not logged in!");
+    }
+
+    const formDataToSend = {
+      name: bookName,
+      isbn: bookISBN,
+      pages: bookPage,
+      cover: bookCover,
+      user_id: pb.authStore.model.id,
+    };
+
+    try {
+      const response = await pb.collection("atay_books").create(formDataToSend);
+
+      if (response.ok) {
+        // Data was successfully uploaded
+        console.log("Book added successfully!");
+      } else {
+        // Handle any errors here
+        console.error(
+          "Failed to add book:",
+          response.status,
+          response.statusText
+        );
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
   return (
     <>
       <h2>New book page</h2>
-      <TextField label="Book name" variant="filled" />
-      <TextField label="ISBN" variant="filled" />
-      <TextField
-        label="Page count"
-        variant="filled"
-        type="number"
-        InputProps={{ inputProps: { min: 0 } }}
-      />
+      <form onSubmit={newBookSubmit}>
+        <TextField
+          name="bookName"
+          value={formData.bookName}
+          onChange={handleChange}
+          label="Book name"
+          variant="filled"
+        />
+        <TextField
+          name="bookISBN"
+          value={formData.bookISBN}
+          onChange={handleChange}
+          label="ISBN"
+          variant="filled"
+        />
+        <TextField
+          name="bookPage"
+          value={formData.bookPage}
+          onChange={handleChange}
+          label="Page count"
+          variant="filled"
+          type="number"
+          InputProps={{ inputProps: { min: 0 } }}
+        />
+        <input
+          type="file"
+          accept=".png,.jpeg,.jpg"
+          onChange={handleCoverChange}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Add new book
+        </Button>
+      </form>
     </>
   );
 };
--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -7,7 +7,7 @@ const BookItem: React.FC<BookItemProps> = ({ name, cover }) => {
   return (
     <>
       <h3>{name}</h3>
-      <img src={cover} alt={`${name} cover`} />
+      <img src={cover} alt={`${name} cover`} width={160} />
     </>
   );
 };
