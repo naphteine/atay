c5405c9371868facd0f90df23cc09d7a4bc89ecb
gguilt <58526116+naphteine@users.noreply.github.com>
2023-09-23 21:29:15 +0300

    Move data fetching to server side with no cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import LastAddedBooks from "@/components/LastAddedBooks";
 import styles from "@/styles/pages/Home.module.css";
+import PocketBase from "pocketbase";
 
-export default function Home() {
+export const fetchCache = "default-no-store";
+
+async function getData() {
+  const pb = new PocketBase("https://duga1.xyz");
+  const data = pb.collection("atay_books").getList(1, 20);
+
+  if (!data) {
+    throw Error("No data!");
+  }
+
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <>
       <main className={styles.main}>
-        <LastAddedBooks />
+        {data &&
+          data.items.map((book) => {
+            return <li key={book.id}>{book.name}</li>;
+          })}
       </main>
     </>
   );
